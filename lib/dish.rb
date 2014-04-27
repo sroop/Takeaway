1cc48404e00f4c0f122ef2887c94c5a470ff2610
Sroop <sroopsunar@hotmail.com>
2014-04-27 13:19:27 +0100

    Dish: has a price

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,8 +1,9 @@
 class Dish
 
-	attr_reader :name
+	attr_reader :name, :price
 
-	def initialize(name)
+	def initialize(name, price)
 		@name = name
+		@price = price
 	end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,10 +2,14 @@ require 'dish'
 
 describe 'Dish' do
 	
-	let(:dish) { Dish.new("Salmon Sashimi") }
+	let(:dish) { Dish.new("Salmon Sashimi", 11) }
 
 	it 'knows the name of the dish' do
 		expect(dish.name).to eq("Salmon Sashimi")
+	end
+
+	it 'knows the price of the dish' do
+		expect(dish.price).to eq(11)
 	end
 
 end
